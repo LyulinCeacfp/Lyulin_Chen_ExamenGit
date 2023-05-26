7cf2f5f644598bc15288ecf0ee6b0d368de19fa7
LyulinChen-AlumnoCEA <LyulinChen-AlumnoCEA@CEACFP10399>
2023-05-26 13:18:21 +0200

    reintentando provocar conflicto

--- Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
+++ Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
@@ -8,7 +8,7 @@ public class clase {
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
-		System.out.println("Hola mundo, valor es: "+valosfsagfsagasr);
+		System.out.println("Hola mundo, valor es: "+que es esofoas);
 	}
 	
 	
