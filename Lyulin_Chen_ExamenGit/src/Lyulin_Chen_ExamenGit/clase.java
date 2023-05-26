d8674216c7d35b5a63a0a1fd9cb6b63baa737aba
LyulinChen-AlumnoCEA <LyulinChen-AlumnoCEA@CEACFP10399>
2023-05-26 13:04:47 +0200

    añadiendo hola mundo

--- Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
+++ Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
@@ -5,6 +5,10 @@ class clase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		System.out.println("Hola mundo");
+		
 	}
 
 }
