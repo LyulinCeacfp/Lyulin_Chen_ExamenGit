75bd33faf5b951d7d301772edc54eb3b19effa89
LyulinChen-AlumnoCEA <LyulinChen-AlumnoCEA@CEACFP10399>
2023-05-26 13:15:54 +0200

    añadido para que pinta el valor

--- Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
+++ Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
@@ -1,15 +1,16 @@
 package Lyulin_Chen_ExamenGit;
 
 public class clase {
-
+	public static int valor = 10;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo asgfhuasghdjuasgjbajg");
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
-		
+		System.out.println("Hola mundo, valor es: "+valor);
 	}
-	public int valor = 10;
+	
+	
 
 }
