6c0cb53eb0c5a2014f4083419b9b94a98bf1f728
LyulinChen-AlumnoCEA <LyulinChen-AlumnoCEA@CEACFP10399>
2023-05-26 13:12:25 +0200

    valor nuevo

--- Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
+++ Lyulin_Chen_ExamenGit/src/Lyulin_Chen_ExamenGit/clase.java
@@ -1,14 +1,15 @@
 package Lyulin_Chen_ExamenGit;
 
-class clase {
+public class clase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo asgfhuasghdjuasgjbajg");
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
 		
 	}
+	public int valor = 10;
 
 }
